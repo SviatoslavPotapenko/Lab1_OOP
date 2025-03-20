9601062fd9a91b882862a03386becfd27e03e341
SviatoslavPotapenko <Sviatoslav.Potapenko@lnu.edu.ua>
2025-03-20 10:57:12 +0200

    Update price.cpp

--- lab1_OOP/price.cpp
+++ lab1_OOP/price.cpp
@@ -1,33 +1,32 @@
 #include "price.h"
-#include <cmath>
 
-void normal(int& hrn, int& kop) {
-    if (kop >= 100) {
-        hrn += kop / 100;
-        kop %= 100;
+void adjustValues(int& hryvnias, short& kopecks) {
+    if (kopecks >= 100) {
+        hryvnias += kopecks / 100;
+        kopecks %= 100;
     }
 }
 
-void addPrices(int h1, int k1, int h2, int k2, int& resultH, int& resultK) {
-    resultH = h1 + h2;
-    resultK = k1 + k2;
-    normal(resultH, resultK);
+void sumCosts(Price& total, const Price& additional) {
+    total.hryvnias += additional.hryvnias;
+    total.kopecks += additional.kopecks;
+    adjustValues(total.hryvnias, total.kopecks);
 }
 
-void multiplyPrices(int h, int k, double multiplier, int& resultH, int& resultK) {
-    double totalKop = (h * 100 + k) * multiplier;
-    int roundedTotal = static_cast<int>(round(totalKop));
-    resultH = roundedTotal / 100;
-    resultK = roundedTotal % 100;
+void multiplyCost(Price& price, double factor) {
+    double totalKopecks = (price.hryvnias * 100 + price.kopecks) * factor;
+    int roundedTotal = static_cast<int>(round(totalKopecks));
+    price.hryvnias = roundedTotal / 100;
+    price.kopecks = roundedTotal % 100;
+    adjustValues(price.hryvnias, price.kopecks);
 }
 
-void roundToNatBank(int& hryvnia, int& kopiykas) {
-    int remainder = kopiykas % 10;
-    if (remainder < 5)
-        kopiykas -= remainder;
-    else
-        kopiykas += (10 - remainder);
-
-    normal(hryvnia, kopiykas);
+void roundToBankRules(int& hryvnias, short& kopecks) {
+    int remainder = kopecks % 10;
+    if (remainder < 5) {
+        kopecks -= remainder;
+    } else {
+        kopecks += (10 - remainder);
+    }
+    adjustValues(hryvnias, kopecks);
 }
-
